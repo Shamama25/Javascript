bac621f71a49ae5f254fe55166694875b4132434
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-01-14 17:53:45 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node js, not browser
+
+console.log("Shamama");
+let name = "Shamama"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigInt 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => useful for finding uniqueness
+
+//objects
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
