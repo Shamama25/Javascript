42ce6d2d6e76ead85caf2c71129f0aa5abb02564
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-02-03 18:34:54 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const pets = ["cats", "dogs", "parrots"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.pop(0)
+// myArr.unshift(9) // adds in start of array
+// myArr.shift()
+
+// console.log(myArr.includes(4))
+// console.log(myArr.indexOf(4))
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+//slice, splice
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)  //  returns section of array
+console.log(myn1);
+console.log("B", myArr);
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
