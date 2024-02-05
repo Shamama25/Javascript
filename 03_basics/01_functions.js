467ad6f2bd7b06894e5eab23bbb56dbbf1e8d9fc
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-02-05 16:36:05 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,12 +25,37 @@ function addTwoNumbers(n1, n2){
 const result = addTwoNumbers(5,4)
 // console.log("Result: ", result);
 
-function loginUserMessage(username){
-    if(!username){
-        console.log("Please enter username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username){
+//     if(!username){
+//         console.log("Please enter username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 // console.log(loginUserMessage("Shamama"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){   //rest operator ...num(means pack items in bundle and return)
+    return num1
+}
+// console.log(calculateCartPrice(2,4,6));
+
+const user = {
+    username: "shamama",
+    price: 500
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// console.log(handleObject(user));
+handleObject({
+    username: "sam",
+    price: 200
+})
+
+const arr = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+// console.log(returnSecondValue(arr));
+// console.log(returnSecondValue([200,400,500,600]));
