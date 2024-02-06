e3edcbd0aa75dbab297b08d1340d03f1ef53e50a
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-02-06 17:41:13 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+let a = 100
+// const b = 20
+// var c = 300
+
+if(true){
+    let a = 10
+    // const b = 20
+    // c = 30
+    // console.log("Inner: " ,a);
+}
+
+for(let i=0; i<Array.length; i++){
+    const element = array[i];
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
