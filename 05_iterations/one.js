7ef40ce42f15dbdff2a4107e14c6c3016fe80707
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-02-10 11:48:24 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,27 @@
+//  for
+
+// for(let index=0; index<Array.length; index++){
+//     const element = index;
+//     // console.log(element);
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+// for(let i=0; i<=10; i++){
+//     console.log(`Outer loop value ${i}`);
+//     for(let j=0; j<=10; j++){
+//         console.log(`Inner loop value ${j}`);
+//     }
+// }
+
+//  break and continue
+
+for(let i=1; i<=20; i++){
+    if(i == 5){
+        console.log('Detected 5');
+        continue;
+    }
+    console.log(`Value of i id ${i}`);
+}
