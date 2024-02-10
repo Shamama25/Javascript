efd6b5ec6577b06d855fd382abba697ac8268628
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-02-10 11:38:33 +0000

    nullish and ternary operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -21,3 +21,19 @@ const embtyObj = {}
 if(Object.keys(embtyObj).length === 0){
     console.log("Object is empty");
 }
+
+//  Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+console.log(val1);
+
+//  Ternary Operator
+
+//  condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
